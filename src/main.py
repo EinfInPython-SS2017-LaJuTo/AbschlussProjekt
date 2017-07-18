9f46f552d8e567f97d996cbd75ad44cec70bf734
Garbaz <garbaz@t-online.de>
2017-07-18 15:27:35 +0200

    Started gui implementation

--- src/main.py
+++ src/main.py
@@ -2,27 +2,30 @@
 
 import pygame as pg
 
+framerate = 60
 background_image_path = "res/background.png"
 
-def main():
+if __name__ == "__main__":
     pg.init()
 
     background_image = pg.image.load(background_image_path)
 
     main_surface = pg.display.set_mode((background_image.get_width(),background_image.get_height()))
+    
+    clock = pg.time.Clock()
 
     while True:
-        ev = pg.event.poll()  
+        deltatime = clock.tick(framerate)
+        
+        ev = pg.event.poll()
         if ev.type == pg.QUIT:
             break
         elif ev.type == pg.KEYDOWN:
-            if ev.key == ord('q'):
+            if ev.key == pg.K_q:
                 break
         
         main_surface.blit(background_image,(0,0))
         
         pg.display.flip()
 
-    pg.quit()     # Once we leave the loop, close the window.
-
-main()
+    pg.quit()
