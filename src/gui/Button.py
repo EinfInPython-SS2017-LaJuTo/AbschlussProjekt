df26b1209acfd0b88fd85fecb96fb0b8e22a9fd3
Garbaz <garbaz@t-online.de>
2017-07-19 13:07:55 +0200

    Added click handling and decent drawing to Button

--- src/gui/Button.py
+++ src/gui/Button.py
@@ -8,19 +8,25 @@ class Button(pg.Rect):
     line_width = None
     pressed = False
     
-    def __init__(self, left, top, width, height, bg_color=pg.Color(255,255,255,255), line_color=pg.Color(0,0,0,255), line_width=1):
-        super().__init__(left,top,width,height)
+    def __init__(self, left, top, width, height, bg_color=pg.Color(200,200,200), line_color=pg.Color(0,0,0), line_width=1):
+        super().__init__(left, top, width, height)
         self.bg_color = bg_color
         self.line_color = line_color
         self.line_width = line_width
 
-    def press(self):
-        self.pressed = True
+    def check_press(self,x,y):
+        if self.collidepoint(x,y):
+            self.pressed = True
 
     def release(self):
         self.pressed = False
 
-    def draw(self,surface):
-        surface.fill(self.bg_color,rect=self)
-        pg.draw.rect(surface,self.line_color,self,self.line_width)
-        pg.draw.rect(surface, self.line_color, self.inflate(-4*self.line_width, -4*self.line_width), self.line_width)
+    def draw(self, surface):
+        if self.pressed:
+            surface.fill(self.bg_color-pg.Color(32,32,32), rect=self)
+            pg.draw.rect(surface, self.line_color, self, self.line_width)
+            pg.draw.rect(surface, self.line_color, self.inflate(-8*self.line_width, -8*self.line_width).move(self.line_width, self.line_width), self.line_width)
+        else:
+            surface.fill(self.bg_color,rect=self)
+            pg.draw.rect(surface, self.line_color, self, self.line_width)
+            pg.draw.rect(surface, self.line_color, self.inflate(-8*self.line_width, -8*self.line_width).move(-self.line_width, -self.line_width), self.line_width)
