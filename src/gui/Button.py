7ab30457685ee6ab18d51797d873ae2a53cb38e8
Garbaz <garbaz@t-online.de>
2017-07-19 11:48:07 +0200

    ...

---
+++ src/gui/Button.py
@@ -0,0 +1,25 @@
+
+import pygame as pg
+
+class Button(pg.Rect):
+    
+    bg_color = None
+    line_color = None
+    line_width = None
+    pressed = False
+    
+    def __init__(self, left, top, width, height, bg_color=pg.Color(255,255,255,255), line_color=pg.Color(0,0,0,255), line_width=2):
+        super().__init__(left,top,width,height)
+        self.bg_color = bg_color
+        self.line_color = line_color
+        self.line_width = line_width
+
+    def press(self):
+        self.pressed = True
+
+    def release(self):
+        self.pressed = False
+
+    def draw(self,surface):
+        surface.fill(self.bg_color,rect=self)
+        pg.draw.rect(surface, self.line_color, self.inflate(-3*self.line_width, -3*self.line_width).move(self.line_width, self.line_width), self.line_width)
