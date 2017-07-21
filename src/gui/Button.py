c43a19e0df7206d31edead00bdcc8e001366e90c
Garbaz <garbaz@t-online.de>
2017-07-21 16:16:58 +0200

    Label now supports multiples lines of text and Button propperly uses Label as a parent class.

--- src/gui/Button.py
+++ src/gui/Button.py
@@ -17,19 +17,13 @@ class Button(Label):
         self.pressed = False
 
     def draw(self, surface):
-        text_font,text_render = None, None
-        if self.text != "":
-            text_font = pg.font.Font(None,self.text_size)
-            text_render = text_font.render(self.text, True, self.text_color)
         if self.pressed:
             surface.fill(self.bg_color-pg.Color(32,32,32), rect=self)
             pg.draw.rect(surface, self.line_color, self, self.line_width)
             pg.draw.rect(surface, self.line_color, self.inflate(-8*self.line_width, -8*self.line_width).move(self.line_width, self.line_width), self.line_width)
-            if text_font != None and text_render != None:
-                surface.blit(text_render,text_render.get_rect(center=self.center).move(self.line_width,self.line_width))
+            self._draw_text(surface, self.line_width, self.line_width)
         else:
             surface.fill(self.bg_color,rect=self)
             pg.draw.rect(surface, self.line_color, self, self.line_width)
             pg.draw.rect(surface, self.line_color, self.inflate(-8*self.line_width, -8*self.line_width).move(-self.line_width, -self.line_width), self.line_width)
-            if text_font != None and text_render != None:
-                surface.blit(text_render,text_render.get_rect(center=self.center).move(-self.line_width,-self.line_width))
+            self._draw_text(surface, -self.line_width, -self.line_width)
--- src/gui/Label.py
+++ src/gui/Label.py
@@ -19,22 +19,43 @@ class Label(pg.Rect):
         self.line_width = line_width
         self.text = text
         self.text_color = text_color
-        if text_size == None:
-            text_size = height//2
         self.text_size = text_size
         self.text_pos = text_pos
 
-    def draw(self, surface):
-        text_font,text_render = None, None
-        if self.text != "": # Only render the text if necessary
-            text_font = pg.font.Font(None,self.text_size)
-            text_render = text_font.render(self.text, True, self.text_color)
-        
+    def draw(self, surface): 
         surface.fill(self.bg_color, rect=self)
         pg.draw.rect(surface, self.line_color, self, self.line_width)
-        if text_render != None:
-            if text_pos == None: # If text_pos = None, draw the text centered
-                surface.blit(text_render,text_render.get_rect(center=self.center))
-            else: # Otherwise at the given position
-                surface.blit(text_render, text_pos)
-                
+        self._draw_text(surface)
+
+    def _draw_text(self, surface, delta_x=0, delta_y=0):
+        text_font = None
+        text_renders = []
+        if self.text != "": # Only render the text if necessary
+            text_lines = self.text.splitlines()
+            text_font = None
+            if self.text_size != None:
+                text_font = pg.font.Font(None,self.text_size)
+            else:
+                text_font = pg.font.Font(None,self.height//(len(text_lines)+1)) # TODO: Not a great solution...
+            for l in self.text.splitlines():
+                text_renders.append(text_font.render(l, True, self.text_color))
+        if len(text_renders) > 0:
+            total_text_height = 0
+            max_text_width = 0
+            for r in text_renders:
+                total_text_height += r.get_height()
+                if r.get_width() > max_text_width:
+                    max_text_width = r.get_width()
+            full_text = pg.Surface((max_text_width, total_text_height),pg.SRCALPHA)
+            curr_y = 0
+            if self.text_pos == None:
+                for r in text_renders:
+                    full_text.blit(r, r.get_rect(centerx=full_text.get_rect().centerx).move(0,curr_y))
+                    curr_y += r.get_height()
+                surface.blit(full_text,full_text.get_rect(center=self.center).move(delta_x, delta_y))
+            else:
+                for r in text_renders:
+                    full_text.blit(r, r.get_rect().move(0,curr_y))
+                    curr_y += r.get_height()
+                surface.blit(full_text,pg.Rect(self.text_pos,(0,0)).move(self.left,self.top).move(delta_x, delta_y))
+
