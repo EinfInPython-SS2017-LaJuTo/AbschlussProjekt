39bf8be8bc20f8ab74bb5a19ed1596b15762129f
Garbaz <garbaz@t-online.de>
2017-10-04 11:34:02 +0200

    Implemented sidemenu

--- src/gui/Sidemenu.py
+++ src/gui/Sidemenu.py
@@ -6,34 +6,62 @@ class Sidemenu(pg.Rect):
 
     # atitributes:
         # bg_color
-        # towers
         # label_health
         # label_money
         # {pg.Rect}
-    
-    def __init__(self, left, top, width, height, bg_color, towers):
+
+        # gameengine   # A reference to the gameengine to call add_tower and access the tower_types
+
+        # tower_keys   # A sorted list of tower_types.keys() from the gameengine
+        # tower_images # A dictionary
+    # constants:
+        # _label_height
+        # _label_y_margin
+        # _tower_y_start
+        # _tower_wh
+
+    def __init__(self, left, top, width, height, bg_color, towers, gameengine):
+        self._label_height = 50
+        self._label_y_margin = 5
+        self._tower_y_start = 2*(_label_height+_label_y_margin)
+        self._tower_wh = width//2
+
         super().__init__(self, left, top, width, height)
         self.bg_color = bg_color
-        self.towers = towers
-        self.label_health = Label(5, 5, self.width, 50)
-        self.label_money = Label(5, 55, self.width, 50)
-        self.next_tower = None
+        self.label_health = Label(0,_label_y_margin, self.width, _label_height)
+        self.label_money = Label(0, _label_height+_label_y_margin, self.width, _label_height)
+        self.gameengine = gameengine
+        self.tower_keys = sorted(self.gameengine.tower_types.keys())
+        self.tower_images = {}
+        for k in self.tower_keys:
+            self.tower_images[k] = pg.transform.scale(self.gameengine.tower_types[k][0], (self._tower_wh,self._tower_wh))
     
     def check_press(self, pos):
         if self.collidepoint(pos):
             x,y = pos
-            for t in towers:
-                x_t,y_t = t.pos
-                if math.abs(x_t-x) <= t.radius and math.abs(y_t-y) <= t.radius:
-                    self.next_tower = None#TODO
+            index = ((y-self._tower_y_start)//(self._tower_wh))*2 + ((x-self.left) >= self._tower_wh) # Calculating the index in tower_keys 
+            if(index < len(self.tower_keys)):
+                self.gameengine.add_tower(self.tower_keys[index])
+
+
 
     def release(self):
-        pass #TODO (?)
+        pass
 
     def draw(self, surface):
         surface.fill(self.bg_color, rect=self)
         self.label_health.draw(surface)
         self.label_money.draw(surface)
-        for t in self.towers:
-            surface.blit(t.image,(0,0))#TODO
+        
+        x = self.left
+        y = self._tower_y_start
+
+        for k in self.tower_keys:
+            surface.blit(self.tower_images[k],(x,y))
+
+            if x == self.left:
+                x = self.left+self._tower_wh
+            else:
+                x = self.left
+                y += self._tower_wh
 
